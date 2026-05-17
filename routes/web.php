bfad1a45cea3209dd00198306fe641a5b3c169e2
Liam Texeira <liamtex1@gmail.com>
2026-05-17 13:15:17 -0300

    a

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', [LandingController::class, 'index'])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'Dashboard')->name('dashboard');
+    /* Route::inertia('dashboard', 'Dashboard')->name('dashboard'); */
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
 
 Route::middleware(['auth', 'role:cliente'])->prefix('cliente')->name('cliente.')->group(function () {
