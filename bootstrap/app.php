443d1a5c51f6c98ac400d99266f727ba03098352
Juan Siecola <juanesiecola@gmail.com>
2026-06-25 14:28:57 -0300

    cambios en middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,11 +14,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-         
-    $middleware->alias([
-        'role' => \App\Http\Middleware\CheckRole::class,
-    ]);
-    
+        $middleware->trustProxies(at: '*');
+
+        $middleware->alias([
+            'role' => \App\Http\Middleware\CheckRole::class,
+        ]);
+
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
